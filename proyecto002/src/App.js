071519f138644f8d931c377076b9cd70583ff5ae
Surteys <60399602+Surteys@users.noreply.github.com>
2022-09-26 18:56:24 -0500

    Asignación de valores a atributos de un elemento HTML
    -Asignación de valores a atributos de un elemento HTML

--- proyecto002/src/App.js
+++ proyecto002/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function retornarAleatorio(){
@@ -6,23 +5,18 @@ function retornarAleatorio(){
 }
 
 function App() {
-  const siglo = 21;
-  const persona = {
-    nombre: 'Juan',
-    edad: 34
-  }
+  const buscadores=[
+    'https://www.google.com',
+    'https://www.bing.com',
+    'https://www.yahoo.com'
+  ];
+
+
   return (
     <div>
-      <h1>Titulo Nivel 1</h1>
-      <hr/>
-      <p>Estamos en el siglo {siglo}</p>
-      <h3>Acceso a un objeto</h3>
-      <p>{persona.nombre} tiene {persona.edad} años</p>
-      <h3>Llamada a un metodo</h3>
-      <p>Un valor aleatorio llamando a un metodo:</p>
-      {retornarAleatorio()}
-      <h3>Calculo inmediato de expresiones</h3>
-      3+3= {3+3}
+      <a href={buscadores[0]}>Google</a><br></br>
+      <a href={buscadores[1]}>Bing</a><br></br>
+      <a href={buscadores[2]}>Yahoo</a><br></br>
     </div>
   );
 }
