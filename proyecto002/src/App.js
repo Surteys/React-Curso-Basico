6a7611189a7f2e3988f1183d7fbaaf5fce8c8d0d
Surteys <60399602+Surteys@users.noreply.github.com>
2022-09-29 01:34:24 -0500

    Retornar contenido JSX desde una función personalizada
    Retornar contenido JSX desde una función personalizada

--- proyecto002/src/App.js
+++ proyecto002/src/App.js
@@ -1,22 +1,18 @@
 import './App.css';
 
-function retornarAleatorio(){
-  return Math.trunc(Math.random()*10)
+function mostrartitulo(titulo){
+  return (
+    <h1>
+      {titulo}
+    </h1>
+  );
 }
 
 function App() {
-  const buscadores=[
-    'https://www.google.com',
-    'https://www.bing.com',
-    'https://www.yahoo.com'
-  ];
-
-
   return (
     <div>
-      <a href={buscadores[0]}>Google</a><br></br>
-      <a href={buscadores[1]}>Bing</a><br></br>
-      <a href={buscadores[2]}>Yahoo</a><br></br>
+      {mostrartitulo("¡Hola... Mundo!")}
+      {mostrartitulo("¡Adiós... Mundo!")}
     </div>
   );
 }
